20b58631a3f564a1ba0e2d53c8888b900d586eb2
Victor Nițu <victor@debian.org.ro>
2013-04-14 14:38:35 +0300

    Added tmpl_url constant

--- protected/GENERAL/core/index.php
+++ protected/GENERAL/core/index.php
@@ -1,6 +1,7 @@
 <?php
 global $core;
-define('tmpl_inc',publicPath."fw/LOCALS/{$core->mainModel}/tmpl_{$core->mainTemplate}/tmpl/");
+define('tmpl_inc', publicPath . "fw/LOCALS/{$core->mainModel}/tmpl_{$core->mainTemplate}/tmpl/");
+define('tmpl_url', publicURL . "fw/LOCALS/{$core->mainModel}/tmpl_{$core->mainTemplate}/");
 
 
 require_once(fw_pubPath.'GENERAL/core/tmpl/header.php');
