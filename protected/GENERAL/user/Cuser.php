c9223c680a465499a714da5a18a1f8141f5d69dc
Victor Nițu <victor@debian.org.ro>
2013-07-24 19:10:55 +0300

    MDB2 support and session storage
     * full MDB2 support
     * integrated PEAR HTTP_Session2
     * new database wrapper class for the default DB object

--- protected/GENERAL/user/Cuser.php
+++ protected/GENERAL/user/Cuser.php
@@ -1,5 +1,6 @@
 <?php
-class Cuser extends permissions{
+class Cuser extends permissions
+{
 
     // comming from $_SESSION['userData'] & seted by CauthManager
     public $uid = 0;                // user ID
@@ -23,32 +24,31 @@ class Cuser extends permissions{
 
     // trebuie mutate undeva in Cblog , aceasta este o clasa de generala de user
     function getRecordPermss(&$mod, $uidRec){
-        #echo "Incerc sa iau permisiunile pentru record";
-        if($this->uclass=='webmaster'){
+        // echo "Incerc sa iau permisiunile pentru record";
+        if ($this->uclass=='webmaster') {
             $mod->editRecordPermss = true;
             $mod->pubPermss = true;
             $mod->webmPermss = true;
-        }
-        elseif($this->uid == $uidRec){
+        } elseif ($this->uid == $uidRec) {
 
 
-             $mod->editRecordPermss = true;
-             if($this->uclass=='publisher')
-                 $mod->pubPermss = true;
-            # declarate deja ca default in $mod;
-            # $mod->pubPermss = ($this->uclass=='publisher' ? true : false);
-        }
-        else{
+            $mod->editRecordPermss = true;
+            if ($this->uclass=='publisher') {
+                $mod->pubPermss = true;
+            }
+            // declarate deja ca default in $mod;
+            // $mod->pubPermss = ($this->uclass=='publisher' ? true : false);
+        } else {
             $mod->ED = 'not';
-            # declarate deja ca default in $mod;
-            #$mod->editRecordPermss = false;
-            #$mod->pubPermss = false;
+            // declarate deja ca default in $mod;
+            // $mod->editRecordPermss = false;
+            // $mod->pubPermss = false;
         }
 
 
-        # adica daca userul este acelasi de userul care a edita articolul
-        # sau daca are permisiuni de masterEditor
-        # atunci poate edita articolul
+        // adica daca userul este acelasi de userul care a edita articolul
+        // sau daca are permisiuni de masterEditor
+        // atunci poate edita articolul
     }
 
         /**
@@ -60,10 +60,14 @@ class Cuser extends permissions{
          */
     function get_EDrecord(&$mod, $uidRec){
 
-        if(isset($this->admin) && $this->admin != NULL){
-            if($mod->editRecords_Permss)   return '';
-            elseif($uidRec == $this->uid)  return '';
-            else return 'not';
+        if (isset($this->admin) && $this->admin != null) {
+            if ($mod->editRecords_Permss) {
+                return '';
+            } elseif ($uidRec == $this->uid) {
+                return '';
+            } else {
+                return 'not';
+            }
 
         }
 
@@ -73,54 +77,63 @@ class Cuser extends permissions{
 
     function getRecordsPermss(&$mod=''){
 
-        # daca este pe profilul personal
-        # daca este masterEditor, admin ceva...
-        # deocamdata toate se pot edita si sterge
-        # variabila EDrecord este cea de care trebuie avut grija
-        #$editRecords
+        // daca este pe profilul personal
+        // daca este masterEditor, admin ceva...
+        // deocamdata toate se pot edita si sterge
+        // variabila EDrecord este cea de care trebuie avut grija
+        //$editRecords
 
-        #daca i se transmite un pointer la un obiect va seta si o variabila
-        # daca nu va da return doar true sau false
+        //daca i se transmite un pointer la un obiect va seta si o variabila
+        // daca nu va da return doar true sau false
 
-        if($this->uclass=='webmaster'){
-            if($mod!='' && is_object($mod))
-                 $mod->editRecords_Permss = true;
+        if ($this->uclass=='webmaster') {
+            if ($mod!='' && is_object($mod)) {
+                $mod->editRecords_Permss = true;
+            }
             return true;
-        }
-        else
+        } else {
             return false;
+        }
 
 
 
     }
     function getCommentsPermss($uidRec){
 
-        if($this->uid == $uidRec) return true;
-        elseif($this->uclass=='webmaster') return true;
-        else
+        if ($this->uid == $uidRec) {
+            return true;
+        } elseif ($this->uclass=='webmaster') {
+            return true;
+        } else {
             return false;
+        }
 
     }
+
     function checkOwn($uidRec){
 
-         if ($this->uid == $uidRec) {
-             return true;
-         } else {
-             return false;
-         }
+        if ($this->uid == $uidRec) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
     public function get_avatar( $email, $s = 80, $d = 'mm', $r = 'g',
         $img = false, $atts = array()
     ) {
-        if (!defined('AVATAR') || AVATAR == FALSE) return 0;
+
+        if (!defined('AVATAR') || AVATAR == false) {
+            return 0;
+        }
+
         $url = 'http://cdn.libravatar.org/avatar/40f8d096a3777232204cb3f796c577b7?s=80&amp;d=mm';
         $url2  = 'http://cdn.libravatar.org/avatar/';
-        $url2 .= md5( strtolower( trim( $email ) ) );
+        $url2 .= md5(strtolower(trim($email)));
         $url2 .= "?s=$s&amp;d=$d&amp;r=$r";
 
-        if(Toolbox::http_response_code($url2) != '404') {
+        if (Toolbox::http_response_code($url2) != '404') {
             $url = $url2;
             unset($url2);
         }
@@ -131,20 +144,28 @@ class Cuser extends permissions{
             $url .= ' ' . $key . '="' . $val . '"';
             $url .= ' />';
         }
-    return $url;
+        return $url;
     }
 
-    private function getUserData($uid) {
+    private function getUserData($uid)
+    {
         $detailsTable   = 'auth_user_details';
         $statsTable     = 'auth_user_stats';
-        $detailsColumns = array('first_name','last_name','language','country','city','last_ip','creation','last_login');
-        $statsColumns   = array('age','failed_logins','comments_count','articles_count','warn_count');
+        $detailsColumns = array('first_name', 'last_name', 'language',
+                                'country', 'city', 'last_ip', 'creation',
+                                'last_login');
+        $statsColumns   = array('age', 'failed_logins', 'comments_count',
+                                'articles_count', 'warn_count');
 
-        $detailsQuery = "SELECT ";
-        $statsQuery   = "SELECT ";
+        $detailsQuery = $statsQuery = "SELECT ";
 
-        foreach ($detailsColumns as $column) { $detailsQuery .= "$column, "; }
-        foreach ($statsColumns   as $column) { $statsQuery .= "$column, "; }
+        foreach ($detailsColumns as $column) {
+            $detailsQuery .= "$column, ";
+        }
+
+        foreach ($statsColumns   as $column) {
+            $statsQuery .= "$column, ";
+        }
 
         $detailsColumns .= " FROM $detailsTable WHERE uid = '$uid'";
         $statsColumns   .= " FROM $statsTable WHERE uid = '$uid'";
@@ -161,7 +182,7 @@ class Cuser extends permissions{
         if (isset($_SESSION['auth'])/* && count($_SESSION['userData']) > 0*/) {
 
             // dont realy know why it doesn't work
-           /* foreach ($_SESSION['auth'] AS $dbFields => $dbValue) {
+            /* foreach ($_SESSION['auth'] AS $dbFields => $dbValue) {
                 $this->$dbFields = $dbValue;
             }*/
 
@@ -172,20 +193,16 @@ class Cuser extends permissions{
             $this->uname  = $userData->uname;
             $this->email  = $userData->email;
             $this->uclass = $userData->uclass;
-           // $this->first_name  = $userData->first_name;
-           // $this->last_name  = $userData->last_name;
+            // $this->first_name  = $userData->first_name;
+            // $this->last_name  = $userData->last_name;
             $this->permissions  = $userData->permissions;
 
             if (!$this->permissions) {
                 $this->_init_permissions();
                 error_log("[ ivy ] Cuser - _init_ : Citim permisiunile din bd");
-                echo "<br> Cuser _init_ : permissions from db ";
-                var_dump($this->permissions);
 
             } else {
                 $this->permissions =  unserialize($this->permissions);
-                echo "<br> Cuser _init_ : permissions from serialized ";
-                var_dump($this->permissions);
             }
 
         } else {
@@ -194,8 +211,6 @@ class Cuser extends permissions{
 
         // unset l apermisions
         $_SESSION['user'] = $this;
-        echo "<br> Cuser - _init_ :";
-        var_dump($this);
     }
 
 }
